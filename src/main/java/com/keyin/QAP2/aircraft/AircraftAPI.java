52b05d7ba99e0533f58dd1f50e6ae6b24f8ae0ad
AbbyOConnell <116213683+AbbyOConnell@users.noreply.github.com>
2023-12-08 16:40:14 -0330

    cat jam 2

--- src/main/java/com/keyin/QAP2/aircraft/AircraftAPI.java
+++ src/main/java/com/keyin/QAP2/aircraft/AircraftAPI.java
@@ -3,6 +3,6 @@ package com.keyin.QAP2.aircraft;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "passenger",collectionResourceRel = "passenger")
+@RepositoryRestResource(path = "aircraft",collectionResourceRel = "aircraft")
 public interface AircraftAPI extends JpaRepository<Aircraft,Long> {
 }
